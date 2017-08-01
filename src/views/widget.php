b97dd433898fdc03dd7ea9a018e4a63f9b312e6d
md5ffx <fx_9090@mail.ru>
2017-08-01 11:30:42 +0500

    Update widget.php

--- src/views/widget.php
+++ src/views/widget.php
@@ -8,15 +8,15 @@ use yii\helpers\Html;
     <?= Html::hiddenInput('height', $widget->height, ['class' => 'height-input']); ?>
     <div class="new-photo-area" style="height: <?= $widget->cropAreaHeight; ?>; width: <?= $widget->cropAreaWidth; ?>;">
         <div class="cropper-label">
-            <div><?= Yii::t('jcrop', 'Drag Photo');?></div>
-            <div><?= Yii::t('jcrop', 'Or');?></div>
-            <div><?= Html::button(Yii::t('jcrop', 'Select Photo'),['class' => 'btn btn-primary']) ?></div>
+            <div><?= Yii::t('jcrop', 'Drag Photo');?>
+            <?= Yii::t('jcrop', 'Or');?>
+            <?= Html::button(Yii::t('jcrop', 'Select Photo'),['class' => 'btn btn-primary']) ?></div>
         </div>
     </div>
 
     <div class="cropper-buttons">
-        <?= Html::button(Yii::t('jcrop', 'Crop Photo'),['class' => 'btn btn-sm btn-success crop-photo hidden']) ?>
-        <?= Html::button(Yii::t('jcrop', 'Select Another Photo'),['class' => 'btn btn-sm btn-info upload-new-photo hidden']) ?>
+        <?= Html::button(Yii::t('jcrop', 'Crop Photo'),['class' => 'btn btn-xs btn-success crop-photo hidden']) ?>
+        <?= Html::button(Yii::t('jcrop', 'Select Another Photo'),['class' => 'btn btn-xs btn-info upload-new-photo hidden']) ?>
     </div>
 
     <div class="progress hidden" style="width: <?= $widget->cropAreaWidth; ?>;">
