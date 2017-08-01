f572b0f57300d38d192cb533ac93e22cb932ff47
md5ffx <fx_9090@mail.ru>
2017-08-01 11:34:55 +0500

    Create jcrop.php

---
+++ src/messages/ru/jcrop.php
@@ -0,0 +1,13 @@
+<?php
+return [
+    'Drag Photo' => 'Перетащить',
+    'Select Photo' => 'Выбрать',
+    'Or' => '- или -',
+    'Crop Photo' => 'Обрезать',
+    'Select Another Photo' => 'Выбрать другое',
+    'File Size Error' => 'Файл слишком большой. Его размер не может превышать {size} МБ.',
+    'File Extension Error' => 'Разрешены только файлы с этими расширениями: {formats}.',
+    'Missing Attribute' => 'Необходимо указать атрибут «{attribute}»',
+    'Only POST Request' => 'Разрешено только POST-запрос',
+    'Can Not Upload File' => 'Невозможно загрузить файл',
+];
